571777bb54871e5fd2a73232fc8366f7ad1d4075
Rafa Tamura <rafaelatamura@gmail.com>
2023-06-07 15:50:32 -0300

    Code First Completo

--- AulaEntity/Program.cs
+++ AulaEntity/Program.cs
@@ -1,4 +1,5 @@
 ﻿using AulaEntity.DataModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace AulaEntity
 {
@@ -24,7 +25,7 @@ namespace AulaEntity
                 case 1:
                     try
                     {
-                        Console.WriteLine("Insera o nome da pessoa:");
+                        Console.WriteLine("Insira o nome da pessoa:");
                         Pessoa p = new Pessoa();
                         p.nome = Console.ReadLine();
 
@@ -62,16 +63,116 @@ namespace AulaEntity
                 case 2:
                     try
                     {
+                        Console.WriteLine("Informe o ID da pessoa:");
+                        int idPessoa = int.Parse(Console.ReadLine());
 
+                        Pessoa? pAlt = contexto.Pessoas.Find(idPessoa);
+
+                        Console.WriteLine("Informe o nome correto:");
+                        pAlt.nome = Console.ReadLine();
+
+                        contexto.Pessoas.Update(pAlt);
+                        contexto.SaveChanges();
+
+                        Console.WriteLine("Nome alterado com sucesso!");
                     }
                     catch (Exception ex)
                     {
                         Console.WriteLine(ex.Message);
                     }
                     break;
+                case 3:
+                    try
+                    {
+                        Console.WriteLine("Informe o ID da pessoa:");
+                        int id = int.Parse(Console.ReadLine());
+
+                        Pessoa? p = contexto.Pessoas.Find(id);
+
+                        Console.WriteLine("Informe o novo email:");
+
+                        Email e = new Email();
+                        e.email = Console.ReadLine();
+
+                        p.emails.Add(e);
+
+                        contexto.Pessoas.Update(p);
+                        contexto.SaveChanges();
+
+                        Console.WriteLine("Email inserido com sucesso!");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    break;
+                case 4:
+                    try
+                    {
+                        Console.WriteLine("Informe o ID para exclusão:");
+                        int id = int.Parse(Console.ReadLine());
+                        Pessoa p = contexto.Pessoas.Find(id);
+
+                        Console.WriteLine("Confirmar a exclusão de " + p.nome);
+                        Console.WriteLine("E dos seus emails?");
+
+                        foreach (Email item in p.emails)
+                        {
+                            Console.WriteLine("\t" + item.email);
+                        }
+
+                        Console.WriteLine("1 para SIM e 2 para NÃO");
+
+                        if (int.Parse(Console.ReadLine()) == 1)
+                        {
+                            contexto.Pessoas.Remove(p);
+                            contexto.SaveChanges();
+                            Console.WriteLine("Excluido com sucesso!");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    break;
+                case 5:
+                    List<Pessoa> pessoas = (from Pessoa p in contexto.Pessoas select p)
+                                    .Include(pes => pes.emails).ToList<Pessoa>();
+
+                    foreach (Pessoa item in pessoas)
+                    {
+                        Console.WriteLine(item.id + " - " + item.nome);
+
+                        foreach (Email itemE in item.emails)
+                        {
+                            Console.WriteLine("\t" + itemE.email);
+                        }
+
+                        Console.WriteLine();
+                    }
+                    break;
+                case 6:
+                    Console.WriteLine("Informe o ID da Pessoa:");
+                    int idP = int.Parse(Console.ReadLine());
+
+                    //Pessoa pessoa = contexto.Pessoas.Include(p=> p.emails)
+                    //    .Where(p => p.id == id).FirstOrDefault();
+
+                    Pessoa pessoa = contexto.Pessoas.Include(p => p.emails)
+                                        .FirstOrDefault(x => x.id == idP);
+
+                    Console.WriteLine(pessoa.id + " - " + pessoa.nome);
+
+                    foreach (Email item in pessoa.emails)
+                    {
+                        Console.WriteLine("\t" + item.email);
+                    }
+
+
+                    break;
                 default:
                     break;
             }
         }
+        }
     }
-}
